48d2229f985944756113689a28b26ff3da6751d8
Sowon Jang <wish@khu.ac.kr>
2020-10-03 19:22:31 +0900

    2

--- src/main.js
+++ src/main.js
@@ -5,12 +5,14 @@ import naver from 'vue-naver-maps';
 
 Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
-
 Vue.use(naver,{
   clientID: 'tr6k2qy8lh',
   useGovAPI: false,
   subModules:''
 });
+
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
+
